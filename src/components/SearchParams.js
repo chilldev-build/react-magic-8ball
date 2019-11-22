99ec66bc5ef0f6420c49f68663e8526657bae750
Chris Hill <chrishillbuilds@gmail.com>
2019-11-22 14:40:03 -0500

    converted to hooks for state

--- src/components/SearchParams.js
+++ src/components/SearchParams.js
@@ -1,73 +1,51 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import loadData from "../utils/loadData";
 
-class SearchParams extends Component {
-  state = {
-    question: "test",
-    answer: null
-  };
+var questionList = [];
 
-  // async componentDidMount() {
-  //   this.getAnswer();
-  // }
+const SearchParams = () => {
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState([]);
 
-  handleSubmit = async e => {
+  const handleSubmit = async e => {
     e.preventDefault();
-    const { question } = this.state;
-    const url = `https://8ball.delegator.com/magic/JSON/${question}`;
+    console.log(question);
+    const params = encodeURIComponent(question);
+    const url = `https://8ball.delegator.com/magic/JSON/${params}`;
     const answerData = await loadData(url);
 
     console.log("this is the answer:", answerData);
-    const answer = answerData;
-
-    this.setState({
-      answer: answer
-    });
-    console.log(this.state);
+    setAnswer([...answer, answerData.magic]);
+    setQuestion("");
+    questionList.push(answerData);
+    console.log("answer state is: ", answer);
   };
 
-  handleChange = e => {
-    console.log(e.target);
-    const { name, value } = e.target;
-
-    this.setState({
-      [name]: value
-    });
-  };
-
-  // let params = encodeURIComponent("Is today going to be a good day?");
-  // let uri = "https://8ball.delegator.com/magic/JSON/" + params;
-  // fetch(uri)
-  //   .then(response => response.json())
-  //   .then(json => {
-  //     console.log(json);
-  //   });
-
-  render() {
-    const { question, answer } = this.state;
-    const response = answer ? answer.magic.answer : null;
-    const inquiry = answer ? answer.magic.question : null;
-    return (
-      <div className="search-params">
-        <form onSubmit={this.handleSubmit}>
-          <label htmlFor="question">
-            What is your question for the magic eight ball?
-            <input
-              name="question"
-              type="text"
-              value={question}
-              placeholder="question"
-              onChange={this.handleChange}
-            ></input>
-          </label>
-          <button type="submit">Submit</button>
-        </form>
-        <p>
-          {inquiry} {response}
-        </p>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="search-params">
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="question">
+          What is your question for the magic eight ball?
+          <input
+            required
+            name="question"
+            type="text"
+            value={question}
+            placeholder="question"
+            onChange={e => setQuestion(e.target.value)}
+          ></input>
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+      <ul>
+        {answer.map(answerA => (
+          <li key={answerA.question}>
+            {answerA.question}? {answerA.answer}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default SearchParams;
